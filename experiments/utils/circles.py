a2d2c500e83fcaa64da648df6f4b4a0718d876b0
Joshua Morton <joshua.morton13@gmail.com>
2018-02-26 02:55:24 -0500

    Update circles.py with pytype and some tests.

--- experiments/utils/circles.py
+++ experiments/utils/circles.py
@@ -5,26 +5,27 @@ import random
 
 import cv2
 import numpy as np
+from typing import Tuple
 
 
-def boxed_coord(width, height, radius):
-    """Selects a set of coordinates a given distance away from image edges.
+def boxed_coord(width: int, height: int, buffer: int) -> Tuple[int, int]:
+    """Selects a pair of coordinates a given distance away from image edges.
     """
-    x = random.randint(radius, height - radius - 1)
-    y = random.randint(radius, width - radius - 1)
+    x = random.randint(buffer, height - buffer - 1)
+    y = random.randint(buffer, width - buffer - 1)
     return x, y
 
-def random_boxed_circle(width, height, radius):
+def random_boxed_circle(width: int, height: int, radius: int
+        ) -> Tuple[np.ndarray, Tuple[float, float]]:
     """Creates an image containing a circle at a random location.
 
     Args:
-        width: width of output image
-        height: height of output image
-        radius: radius of the resulting circle
+        width: Width of output image.
+        height: Height of output image.
+        radius: Radius of the resulting circle.
     Return:
-        Tuple[image, Tuple[x, y]]
-        where image is the image containing the circle and x,y are the
-        coordinates of the center of the circle
+        A tuple containing a greyscale image of a filled circle and the
+        coordinates of the center of that circle.
     """
     # 3 channel image
     empty = np.zeros((width, height, 3))
@@ -36,7 +37,8 @@ def random_boxed_circle(width, height, radius):
     img = img[:,:,1]
     return img, (x / width, y / height)
 
-def circle(x, y, radius, shape=(10, 10)):
+def circle(x: int, y: int, radius: int, shape: Tuple[int, int] = (10, 10)
+        ) -> np.ndarray:
     width, height = shape
     empty = np.zeros((width, height, 3))
     img = cv2.circle(empty, (x, y), radius, color=(1,1,1), thickness=-1)
---
+++ experiments/utils/test_circles.py
@@ -0,0 +1,29 @@
+from unittest import mock
+import numpy as np
+import pytest
+import random
+
+from experiments.utils import circles
+
+@pytest.mark.parametrize('w,h,r', [
+    (10, 10, 4),
+    (10, 10, 3),
+    (10, 10, 2),
+    (10, 10, 1),
+    (5, 5, 2),
+    (5, 5, 1),
+])
+def test_boxed_cord(w, h, r):
+    """Hypothesis-y testing that the coordinates are reasonable."""
+    x, y = circles.boxed_coord(w, h, r)
+    assert x >= r
+    assert y >= r
+    assert x <= w - r + 1
+    assert y <= h - r + 1
+
+@mock.patch.object(circles, 'boxed_coord')
+def test_random_boxed_circle(mock_coord):
+    mock_coord.return_value = (5,5)
+    circ, (x, y) = circles.random_boxed_circle(10, 10, 2)
+    assert (x, y) == (0.5, 0.5)
+    assert np.sum(circ) == 13  # a 5x5 circle has 13 on pixels
